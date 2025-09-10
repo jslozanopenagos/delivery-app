10e647a88af6d86f2403fdfdb34317774cdfd843
jslozanopenagos <jslozanopenagos@laba.solvd.com>
2025-09-10 13:35:50 -0500

    Order & OrderItem set as record classes

--- src/main/java/com/solvd/delivery/model/orders/OrderItem.java
+++ src/main/java/com/solvd/delivery/model/orders/OrderItem.java
@@ -1,42 +1,8 @@
 package com.solvd.delivery.model.orders;
 
-public class OrderItem {
-    private final long orderItemID;
-    private final long orderID;
-    private long menuItemID;
-    private int quantity;
-
-    public OrderItem(
-            long orderItemID, long orderID,
-            long menuItemID, int quantity
-    ) {
-        this.orderItemID = orderItemID;
-        this.orderID = orderID;
-        this.menuItemID = menuItemID;
-        this.quantity = quantity;
-    }
-
-    public long getOrderItemID() {
-        return orderItemID;
-    }
-
-    public long getOrderID() {
-        return orderID;
-    }
-
-    public long getMenuItemID() {
-        return menuItemID;
-    }
-
-    public void setMenuItemID(long menuItemID) {
-        this.menuItemID = menuItemID;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-}
+public record OrderItem(
+        long orderItemID,
+        long orderID,
+        long restaurantID,
+        long menuItemID,
+        int quantity) {}
